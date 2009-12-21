73ec7866d54b00785601ad186dc6ab6f425c7f83
Bruz Marzolf <bmarzolf@systemsbiology.org>
2009-12-21 11:29:25 -0800

    Reset Boatman.tasks between each spec to address issue with specs that only fail when run via 'rake spec' and not individually

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,5 +7,5 @@ require 'spec/autorun'
 BOATMAN_BIN = File.expand_path(File.dirname(__FILE__) + '/../bin/boatman')
 
 Spec::Runner.configure do |config|
-  
+  config.before(:each) { Boatman.tasks = nil }
 end
